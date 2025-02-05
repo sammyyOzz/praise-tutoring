2012a36ef7a358d8fd8059ddf50169cc928306b9
sammy <sammyoziegbe@gmail.com>
2025-02-05 19:56:58 +0100

    added social links

--- src/components/socials.tsx
+++ src/components/socials.tsx
@@ -6,13 +6,13 @@ import TiktokIcon from "./icons/tiktok";
 function Socials({...props }) {
   return (
     <HStack fontSize="xl" gap={6}>
-      <a href="#" target="_blank">
+      <a href="https://www.instagram.com/proudatutors?igsh=d3h0Nzl4ZzIyaDBx" target="_blank">
         <InstagramIcon w="40px" {...props} />
       </a>
-      <a href="#" target="_blank">
+      <a href="https://tiktok.com/@proudatutors" target="_blank">
         <TiktokIcon w="40px" {...props} />
       </a>
-      <a href="#" target="_blank">
+      <a href="https://youtube.com/@proudatutorsbychimy?si=lBWGPS_1ZHI_5Vvd" target="_blank">
         <YoutubeIcon w="40px" {...props} />
       </a>
     </HStack>
