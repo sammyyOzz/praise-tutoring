fac5091911897b73d07b3a8ce60be95648dac1ad
Samuel Oziegbe <sammyoziegbe@gmail.com>
2026-08-20 11:40:17 +0100

    feat: integrate Facebook Pixel for enhanced tracking and analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Plus_Jakarta_Sans, Fraunces } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 import { Provider } from "@/components/ui/provider";
 import logoYellow from "@/app/images/logo-yellow.png";
@@ -75,6 +76,31 @@ export default function RootLayout({
         />
       </head>
       <body className={`${bodyFont.variable} ${accentFont.variable}`}>
+        {/* Facebook Pixel Code */}
+        <Script id="facebook-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window,document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '1739344723877643');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=1739344723877643&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
+        {/* End Facebook Pixel Code */}
         <Provider>
           {children}
         </Provider>
