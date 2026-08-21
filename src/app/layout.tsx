1428afd65967f534aa9673c50fddbd5a7de2f7da
Samuel Oziegbe <sammyoziegbe@gmail.com>
2026-08-21 10:13:47 +0100

    feat: update Facebook Pixel ID for accurate tracking

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -87,7 +87,7 @@ export default function RootLayout({
             t.src=v;s=b.getElementsByTagName(e)[0];
             s.parentNode.insertBefore(t,s)}(window,document,'script',
             'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', '1739344723877643');
+            fbq('init', '1074562474939889');
             fbq('track', 'PageView');
           `}
         </Script>
@@ -96,7 +96,7 @@ export default function RootLayout({
             height="1"
             width="1"
             style={{ display: "none" }}
-            src="https://www.facebook.com/tr?id=1739344723877643&ev=PageView&noscript=1"
+            src="https://www.facebook.com/tr?id=1074562474939889&ev=PageView&noscript=1"
             alt=""
           />
         </noscript>
